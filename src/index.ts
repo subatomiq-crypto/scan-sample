815bcdbf11222525b99932361eefbd5d430b8601
John B <john@nexys.ch>
2021-01-06 13:18:29 +0100

    remove rsa options

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,6 @@ import ntru from "@subatomiq/ntru";
 const encoder = new TextEncoder();
 const decoder = new TextDecoder("utf-8");
 
-const rsaOptions = {
-  // The standard secure default length for RSA keys is 2048 bits
-  modulusLength: 2048,
-};
-
 export const main = async (message: string): Promise<string> => {
   const crypto = await ntru();
   const { publicKey, privateKey } = await crypto.keyPair();
