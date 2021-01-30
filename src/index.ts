b79c09c9920e1a5aebdc26326ad8f86af7a21b06
John B <johan@nexys.ch>
2021-01-30 11:36:39 +0100

    Revert "to postquantum"

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
-import ntru from "@subatomiq/ntru";
+// example taken from https://www.sohamkamani.com/nodejs/rsa-encryption/
+import crypto from "crypto";
 
 const encoder = new TextEncoder();
 const decoder = new TextDecoder("utf-8");
 
-export const main = async (message: string): Promise<string> => {
-  const crypto = await ntru();
-  const { publicKey, privateKey } = await crypto.keyPair();
+const rsaOptions = {
+  // The standard secure default length for RSA keys is 2048 bits
+  modulusLength: 2048,
+};
+
+export const main = (message: string): string => {
+  const { publicKey, privateKey } = crypto.generateKeyPairSync(
+    "rsa",
+    rsaOptions
+  );
 
   const messageBuffer: Uint8Array = encoder.encode(message);
 
-  const ct = await crypto.encrypt(messageBuffer, publicKey);
-  const d = await crypto.decrypt(ct, privateKey);
+  const ct = crypto.publicEncrypt(publicKey, messageBuffer);
+  const d = crypto.privateDecrypt(privateKey, ct);
 
   return decoder.decode(d);
 };
